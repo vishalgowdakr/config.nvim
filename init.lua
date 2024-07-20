ba9dc1c0a5dbc49793d6ed4ec1774f9dc144bb06
Vishal Gowda K R <vishlgowda096@gmail.com>
2024-07-20 08:32:02 +0530

    changed colorscheme

--- init.lua
+++ init.lua
@@ -283,19 +283,20 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '', group = '[D]ocument' },
+        { '', group = '[S]earch' },
+        { '', group = '[R]ename' },
+        { '', group = '[C]ode' },
+        { '', group = '[W]orkspace' },
+        { '', group = '[T]oggle' },
+        { '', group = 'Git [H]unk' },
+        { '', desc = '', hidden = true, mode = { 'n', 'n', 'n', 'n', 'n', 'n', 'n' } },
       }
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      require('which-key').add {
+        { '', desc = '', mode = 'v' },
+      }
     end,
   },
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,9 +3,53 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'stevearc/oil.nvim',
-  config = function()
-    require('oil').setup()
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-  end,
+  {
+    'stevearc/oil.nvim',
+    config = function()
+      require('oil').setup()
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
+  {
+    'Mofiqul/vscode.nvim',
+    config = function()
+      -- Lua:
+      -- For dark theme (neovim's default)
+      vim.o.background = 'dark'
+
+      local c = require('vscode.colors').get_colors()
+      require('vscode').setup {
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        color_overrides = {
+          vscLineNumber = '#FFFFFF',
+        },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+        },
+      }
+      -- require('vscode').load()
+
+      -- load the theme without affecting devicon colors.
+      vim.cmd.colorscheme 'vscode'
+    end,
+  },
 }
