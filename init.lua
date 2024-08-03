199f88a0fe734aa2a2540a08738ccbbef44a8587
Vishal Gowda K R <vishlgowda096@gmail.com>
2024-08-03 15:14:02 +0530

    this

--- init.lua
+++ init.lua
@@ -914,3 +914,8 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+local lspconfig = require 'lspconfig'
+lspconfig.htmx.setup {
+  filetypes = { 'html', 'templ' },
+}
